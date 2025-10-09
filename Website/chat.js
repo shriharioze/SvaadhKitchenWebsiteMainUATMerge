4b5b8c890ff0ec15b78f5bb94e816ec40ececd30
Shrihari Oze <shriharioze07@gmail.com>
2025-10-09 19:02:22 +0530

    updated domain url in .js file

--- Website/chat.js
+++ Website/chat.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
     appendMessage(text, "user");
     userInput.value = "";
 
-    const res = await fetch("http://127.0.0.1:8000/chat", {
+    const res = await fetch("https://svaadhkitchenwebsite.onrender.com/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message: text }),
