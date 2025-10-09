6dbd4bcfc379527b6de0a364b07aba1f5db03bd4
Shrihari Oze <shriharioze07@gmail.com>
2025-10-09 19:11:38 +0530

    Added root route to fix 404 on Render

--- Backend/main.py
+++ Backend/main.py
@@ -20,6 +20,10 @@ with open(os.path.join(BASE_DIR, "business.json"), "r", encoding="utf-8") as f:
     
 app = FastAPI()
 
+app.get("/")
+def home();
+    return {"message": "Svaadh Kitchen Backend is Live 🍲"}
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # or restrict to your website domain
